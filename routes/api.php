780603afaa2784dff2e5cd590f11610a45c59b81
“altharaa” <aletheamaureen01@gmail.com>
2024-05-30 22:51:24 +0700

    feat : [AM] add auth controller

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\API\AuthController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -18,6 +19,5 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-// Route::post('register', [AuthController::class, 'store']);
-// Route::post('login', [AuthController::class, 'login']);
-// Route::post('logout', [AuthController::class, 'destroy'])->middleware('auth:sanctum');
+Route::post('login', [AuthController::class, 'login']);
+Route::post('logout', [AuthController::class, 'destroy'])->middleware('auth:sanctum');
