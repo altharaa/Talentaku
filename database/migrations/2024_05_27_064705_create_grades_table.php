2b1b0b69b260c06f3ecb418eb537b57120769356
“altharaa” <aletheamaureen01@gmail.com>
2024-06-04 09:55:39 +0700

    fix : [AM] update grade db schema

--- database/migrations/2024_05_27_064705_create_grades_table.php
+++ database/migrations/2024_05_27_064705_create_grades_table.php
@@ -14,6 +14,9 @@ return new class extends Migration
         Schema::create('grades', function (Blueprint $table) {
             $table->id();
             $table->string('name');
+            $table->string('desc');
+            $table->string('unique_code', 5)->unique();
+            $table->boolean('isactive')->default(true);
             $table->unsignedBigInteger('teacher_id');
             $table->timestamps();
 
